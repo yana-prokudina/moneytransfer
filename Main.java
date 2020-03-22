88b63ee14895e5577917a96d204c33270252faba
Yana Prokudina <58114235+yana-prokudina@users.noreply.github.com>
2020-03-22 10:33:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int initial = 2000000000;
+        int added = 500000000;
+        int result = initial + added;
+        System.out.println(result);
+    }
+}
